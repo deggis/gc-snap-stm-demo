2e89740a66264f389f5264b0bbd1a176b72402bd
Heikki Salo <heikki.ao.salo@iki.fi>
2013-07-16 16:28:02 +0300

    Refactored a bit
    Signed-off-by: Heikki Salo <heikki.ao.salo@iki.fi>

--- src/KVBackend.hs
+++ src/KVBackend.hs
@@ -10,6 +10,8 @@ import Snap.Snaplet
 import Snap.Core
 import Control.Monad.IO.Class
 import Control.Applicative
+import Control.Monad
+import Control.Monad.State
 
 import Data.Text.Encoding
 import qualified Data.ByteString as B
@@ -18,48 +20,78 @@ import Data.ByteString (ByteString)
 
 import KeyValueStorage
 
-data KVBackend = KVBackend Storage
+data KVBackend = KVBackend { storage :: Storage }
 
-type KVHandler r = forall a. Handler a KVBackend r
+2type KVHandler r = forall a. Handler a KVBackend r
 
-get :: Storage -> KVHandler ()
-get s = do
---  (KVBackend s) <- get
-  key <- fmap decodeUtf8 <$> getParam "key"
-  case key of
-      Just k  -> do
-          mbDoc <- liftIO $ getDoc s k
-          case mbDoc of
-              Just doc -> writeBS . toStrict $ doc
-              _        -> bad "Not found"
-      Nothing -> bad "No key"
+  
+getWaitDocR :: KVHandler ()
+getWaitDocR = withStorageAndKey $ \(storage,key) -> do
+  doc <- liftIO $ waitDoc storage k
+  writeBS . toStrict $ doc
 
-put :: Storage -> KVHandler ()
-put s = do
-  mbKey <- fmap decodeUtf8 <$> getParam "key"
+getDocR :: KVHandler ()
+getDocR = withStorageAndKey $ \(storage,key) -> do
+  mbDoc <- liftIO $ getDoc storage k
+  case mbDoc of
+      Just doc -> writeBS . toStrict $ doc
+      _        -> bad "Not found"
+
+deleteDocR :: KVHandler ()
+deleteDocR = withStorageAndKey $ \(storage,key) -> do
+  deleted <- liftIO $ delDoc storage key
+  if deleted 
+   then good "Deleted"
+   else bad "Not found"
+
+putDocR :: KVHandler ()
+putDocR = withStorageAndKey $ \(storage,key) -> do
   value <- getRequestBody
-  case mbKey of
-    Just key -> do
-      overwritten <- liftIO $ putDoc s key value
-      good $ if overwritten then "Overwritten" else "Created"
-    Nothing  -> bad "Key missing"
+  overwritten <- liftIO $ putDoc storage key value
+  good $ if overwritten then "Overwritten" else "Created"
 
+getDirR :: KVHandler ()
+getDirR = withStorageAndKey $ \(storage,key) -> do
+  values <- liftIO $ getDocs storage
+  good values
 
-bad,good :: ByteString -> KVHandler ()
-bad msg  = modifyResponse (setResponseStatus 400 "Bad request") >> writeBS msg
-good msg = writeBS msg
-
-routes a = [("/hello", writeText "hello world with snap")
-         ,("/doc/:key",  method GET $ get a)
-         ,("/doc/:key",  method PUT $ put a)
+routes = [("/hello",         writeText "hello world with snap")
+         ,("/doc/:key",      method GET    $ getDocR)
+         ,("/doc/:key/wait", method GET    $ getWaitDocR)
+         ,("/doc/:key",      method DELETE $ deleteDocR)
+         ,("/doc/:key",      method PUT    $ putDocR)
+         ,("/dir",           method GET    $ getDirR)
          ]
 
 initKVBackend :: SnapletInit a KVBackend
 initKVBackend = makeSnaplet "KVBackend" "STM dojo web with Snaplet" Nothing $ do
     storage <- liftIO initStorage
-    addRoutes (routes storage)
+    addRoutes routes
     liftIO $ putDoc storage "avain" "docsi asdasdoasdasda"
     return $ KVBackend storage
+
+
+-- helpers
+
+bad,good :: ByteString -> KVHandler ()
+bad msg  = modifyResponse (setResponseStatus 400 "Bad request") >> writeBS msg
+good msg = writeBS msg
+
+withStorage :: (Storage -> KVHandler ()) -> KVHandler ()
+withStorage handler = storage <$> get >>= \s -> handler s
+
+withKey :: (Text -> KVHandler()) -> KVHandler ()
+withKey handler = do
+  mbKey <- fmap decodeUtf8 <$> getParam "key"
+  case mbKey of
+      Just key  -> handler key
+      Nothing   -> bad "Key missing"
+
+withStorageAndKey :: ((Storage,Text) -> KVHandler ()) -> KVHandler ()
+withStorageAndKey handler =
+    withStorage $ \storage ->
+      withKey $ \key ->
+        handler (storage,key)
 
 
 toStrict :: BL.ByteString -> B.ByteString
